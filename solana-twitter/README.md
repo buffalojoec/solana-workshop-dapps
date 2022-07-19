fb075fab21859b0dec622a0613a7f79e5f319bb1
jpcaulfi <joecaulfield29@yahoo.com>
2022-07-18 22:20:47 -0400

    init

---
+++ solana-twitter/README.md
@@ -0,0 +1,5 @@
+# Solana Twitter
+
+A mock Twitter app built on Solana!   
+
+You can choose to use `native` or `anchor` program development.
---
+++ solana-twitter/native/target/debug/libprogram.d
@@ -0,0 +1,1 @@
+/home/joec/solana-workshop-dapps/solana-twitter/program/target/debug/libprogram.rlib: /home/joec/solana-workshop-dapps/solana-twitter/program/src/lib.rs
