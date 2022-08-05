f2d6faba7da4931c55451e81ece48c865c5e7f45
jpcaulfi <joe.caulfield@solana.org>
2022-08-05 03:19:04 -0500

    mango next config

--- mango-testing/next.config.js
+++ mango-testing/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, options) => {
+    // Important: return the modified config
+    if (!options.isServer) {
+      config.resolve.fallback.fs = false
+    }
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
 }
-
 module.exports = nextConfig
